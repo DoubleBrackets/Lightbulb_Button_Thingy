3d710eb86015bc9b695de089b2b45e016d4075fa
DoubleBrackets <arthurgan29@gmail.com>
2020-08-21 16:12:58 -0700

    commit

--- Assets/Scripts/PlayerButtonScript.cs
+++ Assets/Scripts/PlayerButtonScript.cs
@@ -4,8 +4,31 @@ using UnityEngine;
 
 public class PlayerButtonScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static PlayerButtonScript playerButtonScript;
+
+    public MeshRenderer ren;
+
+    public MeshRenderer lightbulbMat;
+
+    private float emissionMinIntensity = -2;
+    private float emissionmaxIntensity = 2.5f;
+
+    public Color onColor;
+    private Color offColor;
+
+    Rigidbody rb;
+
+    public Light bulbLight;
+
+    private float minForce = 1f;
+    private float winForce = 50f;
+
+    private float minIntensity = 0f;
+    private float maxIntensity = 7f;
+
+    public bool isFlashing = false;
+
+    private void Awake()
     {
         
     }
@@ -13,6 +36,31 @@ public class PlayerButtonScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        isFlashing = true;
+        ren.material.SetColor("_EmissionColor", onColor);
+
+        Vector3 localPos = ren.transform.localPosition;
+        localPos.y += 0.07f;
+        ren.transform.localPosition = localPos;
+        float ratio = Mathf.Min(1f, (force / winForce));
+
+        Color c = lightbulbMat.material.GetColor("_EmissionColor");
+        c *= 10;// emissionMinIntensity + ratio * (emissionmaxIntensity - emissionMinIntensity);
+        lightbulbMat.material.SetColor("_EmissionColor", c);
+
+        for(int x = 0;x <= 30;x++)
+        {
+            bulbLight.intensity = minIntensity + (1 - x / 30f) * ratio * (maxIntensity-minIntensity);
+            yield return new WaitForFixedUpdate();
+        }
+
+        c *= 0;// emissionMinIntensity + ratio * (emissionmaxIntensity - emissionMinIntensity);
+        lightbulbMat.material.SetColor("_EmissionColor", c);
+
+        bulbLight.intensity = 0;
+        isFlashing = false;
+        ren.material.SetColor("_EmissionColor", offColor);
+        localPos.y -= 0.07f;
+        ren.transform.localPosition = localPos;
     }
 }
