eebe56cca854e28e52496961ef72adf7c323831a
DoubleBrackets <arthurgan29@gmail.com>
2020-08-20 18:49:49 -0700

    k

--- Assets/Scripts/CharacterMovementScript.cs
+++ Assets/Scripts/CharacterMovementScript.cs
@@ -4,30 +4,65 @@ using UnityEngine;
 
 public class CharacterMovementScript : MonoBehaviour
 {
-    CharacterController controller;
+    public static CharacterMovementScript characterMovementScript;
 
     Rigidbody rb;
+    MeshCollider coll;
+
+    private float dynFric;
 
     public float speed = 6f;
+    private float accelForce = 40f;
+
+    public float jumpVel = 10f;
 
     float horizontalInput;
     float verticalInput;
 
     float turnVel;
+
+    float jumpTimer = 0;
+
+    public bool canJump = true;
+
+    LayerMask groundedMask;
     // Start is called before the first frame update
     void Awake()
     {
-        controller = gameObject.GetComponent<CharacterController>();
+        characterMovementScript = this;
         rb = gameObject.GetComponent<Rigidbody>();
+        coll = gameObject.GetComponentInChildren<MeshCollider>();
+        dynFric = coll.material.dynamicFriction;
+        groundedMask = LayerMask.GetMask( "Terrain","MoveableObject");
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (jumpTimer > 0)
+            jumpTimer -= Time.deltaTime;
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKeyDown(KeyCode.Space) && jumpTimer <= 0)
+        {
+            if(IsGrounded() && canJump)
+            {
+                jumpTimer = 0.2f;
+                rb.velocity = new Vector3(rb.velocity.x, jumpVel, rb.velocity.z);
+            }
+        }
     }
 
+    private bool IsGrounded()
+    {
+        RaycastHit hit;
+        Physics.BoxCast(transform.position, new Vector3(0.5f,0.4f,0.5f), Vector3.down, out hit,Quaternion.identity, 0.4f,groundedMask);
+        if (hit.collider != null)
+            return true;
+        return false;
+    }
     private void FixedUpdate()
     {
         Vector3 inputVector = new Vector3(horizontalInput, 0, verticalInput).normalized;
@@ -42,16 +77,38 @@ public class CharacterMovementScript : MonoBehaviour
         if (inputVector.magnitude > 0)
         {
 
-            gameObject.GetComponent<Animator>().SetBool("IsMoving", true);
+            //gameObject.GetComponent<Animator>().SetBool("IsMoving", true);
             Vector3 dir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
 
-            gameObject.transform.rotation = Quaternion.Euler(0, Mathf.SmoothDampAngle(transform.rotation.eulerAngles.y, targetAngle, ref turnVel, 0.1f), 0);
+            GameObject target = ObjectManipulateScript.objectManipulateScript.GetTargetObject();
+            if(target == null)
+                gameObject.transform.rotation = Quaternion.Euler(0, Mathf.SmoothDampAngle(transform.rotation.eulerAngles.y, targetAngle, ref turnVel, 0.15f), 0);
+            else
+            {
+                Vector3 dif = target.transform.position - gameObject.transform.position;
+                float newTargetAngle = Mathf.Atan2(dif.x,dif.z) * Mathf.Rad2Deg;
+                gameObject.transform.rotation = Quaternion.Euler(0, Mathf.SmoothDampAngle(transform.rotation.eulerAngles.y, newTargetAngle, ref turnVel, 0.15f), 0);
+            }
 
-            rb.velocity = (dir.normalized * speed);
+            float currentSpeed = new Vector3(rb.velocity.x, 0, rb.velocity.z).magnitude;
+
+            float newSpeed = (rb.velocity + dir.normalized * accelForce * Time.fixedDeltaTime).magnitude;
+            if(newSpeed < speed)
+            {
+                rb.AddForce(dir.normalized * accelForce);
+            }
+            else
+            {
+                rb.velocity = new Vector3(dir.normalized.x * speed, rb.velocity.y, dir.normalized.z * speed);
+            }
+            coll.material.dynamicFriction = 0f;
         }
         else
         {
-            gameObject.GetComponent<Animator>().SetBool("IsMoving", false);
+            rb.velocity = new Vector3(rb.velocity.x * 0.85f,rb.velocity.y,rb.velocity.z * 0.85f);
+            if(IsGrounded())
+                coll.material.dynamicFriction = dynFric;
+            //gameObject.GetComponent<Animator>().SetBool("IsMoving", false);
         }
     }
 }
---
+++ Assets/Scripts/ObjectManipulateScript.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManipulateScript : MonoBehaviour
+{
+    public static ObjectManipulateScript objectManipulateScript;
+    GameObject grabbedObject;
+
+    private float slowdownMultiplier = 0.5f;
+
+    LayerMask raycastMask;
+
+    private bool objectGrabbed = false;
+
+    private void Awake()
+    {
+        objectManipulateScript = this;
+        raycastMask = LayerMask.GetMask("MoveableObject");
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(objectGrabbed)
+            {
+                ReleaseObject();
+            }
+            else
+            {
+                //Raycasts for object
+                RaycastHit hit;
+                Physics.Raycast(transform.position, Quaternion.Euler(transform.rotation.eulerAngles) * Vector3.forward, out hit, 1f, raycastMask);
+                if(hit.collider != null)
+                {
+                    GrabObject(hit.collider.gameObject,(transform.position - hit.collider.transform.position).magnitude+0.1f);
+                }
+            }
+        }
+    }
+
+    private void GrabObject(GameObject obj,float dist)
+    {
+        CharacterMovementScript.characterMovementScript.canJump = false;
+        objectGrabbed = true;
+        grabbedObject = obj;
+        obj.GetComponent<Rigidbody>().mass = 0.01f;
+        obj.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX;
+        CreateJoint(dist);
+
+        CharacterMovementScript.characterMovementScript.speed *= slowdownMultiplier;
+    }
+
+    private void ReleaseObject()
+    {
+        CharacterMovementScript.characterMovementScript.canJump = true;
+        ConfigurableJoint joint = grabbedObject.GetComponent<ConfigurableJoint>();
+        Destroy(joint);
+        grabbedObject.GetComponent<Rigidbody>().mass = 1f;
+        grabbedObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        grabbedObject = null;
+        objectGrabbed = false;
+        CharacterMovementScript.characterMovementScript.speed /= slowdownMultiplier;
+    }
+
+    private void CreateJoint(float dist)
+    {
+        ConfigurableJoint joint = grabbedObject.AddComponent<ConfigurableJoint>();
+        joint.autoConfigureConnectedAnchor = false;
+        joint.connectedAnchor = Vector3.zero;
+        joint.enableCollision = true;
+        var lim = joint.linearLimit;
+        lim.limit = dist;
+        joint.linearLimit = lim;
+        joint.xMotion = ConfigurableJointMotion.Limited;
+        joint.yMotion = ConfigurableJointMotion.Limited;
+        joint.zMotion = ConfigurableJointMotion.Limited;
+        joint.connectedBody = GetComponent<Rigidbody>();
+    }
+
+    public GameObject GetTargetObject()
+    {
+        return grabbedObject;
+    }
+}
