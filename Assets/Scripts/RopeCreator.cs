2aac5d17fdf3f1a42b73d384d4763afc4aa37d1c
DoubleBrackets <arthurgan29@gmail.com>
2020-08-26 14:16:01 -0700

    e

---
+++ Assets/Scripts/RopeCreator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class RopeCreator : MonoBehaviour
+{
+
+    public GameObject ropePrefab;
+
+    void GeneratedRope()
+    {
+
+    }
+}
